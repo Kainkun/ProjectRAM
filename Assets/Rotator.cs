06332a24c01b33b9f2a52bb21386e985f143080e
Kain Shepard <kainkun100@gmail.com>
2021-11-14 13:23:21 -0800

    rotator working now

--- Assets/Rotator.cs
+++ Assets/Rotator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,26 +8,30 @@ public class Rotator : PlayerInteractable
     public int currentPosition = 0;
     public int totalPositions = 4;
     public Transform dial;
-    
+    private float targetRotation;
+    private Coroutine currentCoroutine;
+
+    private void Start()
+    {
+        targetRotation = dial.localEulerAngles.z % 360;
+    }
+
     public override void Interact()
     {
-        StartCoroutine(CR_Rotate());
+        if(currentCoroutine != null)
+            StopCoroutine(currentCoroutine);
+        currentCoroutine = StartCoroutine(CR_Rotate());
         base.Interact();
     }
     
     IEnumerator CR_Rotate()
     {
-        float start = dial.localEulerAngles.z;
-        float target = start + 90;
-
-        yield return null;
-
-        float t = 0;
-        while (t < 1)
+        targetRotation = (targetRotation + 90) % 360;
+        
+        while (Mathf.Abs(Mathf.DeltaAngle(dial.localEulerAngles.z,targetRotation)) > 1)
         {
             var r = dial.localEulerAngles;
-            dial.localRotation = Quaternion.Euler(r.x, r.y, Mathf.LerpAngle(start, target, t));
-            t += Time.deltaTime;
+            dial.localRotation = Quaternion.Euler(r.x, r.y, Mathf.MoveTowardsAngle(dial.localEulerAngles.z, targetRotation, Time.deltaTime * 360));
             yield return null;
         }
     }
